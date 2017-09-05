799874ba44d0f9c802d9d4562e26228c25aa548c
Prasanna Deshappriya <prasanna.14@cse.mrt.ac.lk>
2017-09-05 12:04:53 +0530

    Implement intent update methords with wit.ai apis

--- app.js
+++ app.js
@@ -60,6 +60,8 @@ server.post('/intent/delete', dashboardController.deleteIntent);
 server.post('/entity/create', dashboardController.createEntity);
 server.get('/entity/get', dashboardController.getEntity);
 server.get('/wit/getEntityById', witController.getEntityById);
+server.put('/wit/putEntityById', witController.putEntityById);
+
 
 //Bot on
 bot.on('contactRelationUpdate', function (message) {
--- controller/witController.js
+++ controller/witController.js
@@ -1,22 +1,65 @@
 /**
  * Created by prasanna_d on 9/4/2017.
  */
-let http = require('http');
-var request = require('request');
+let request = require('request');
+
+function print_request(route){
+    let url_domain = 'http://localhost:3000';
+    console.log(url_domain + '/wit' + route);
+}
 
 module.exports = {
     getEntityById: async function(req,res){
+        print_request('getEntityById');
         let entity_name = req.query.entity_name;
+        let wit_token = req.headers.authorization;
+        let wit_content_type = req.headers.content_type;
         if(typeof entity_name==='undefined' || entity_name===''){
-            return res.json(400,{message: 'entity_id is required'});
-        }
+            return res.json(400,{message: 'entity_name is required'});}
+        if(typeof wit_token==='undefined' || wit_token===''){
+            return res.json(400,{message: 'Authorization header is required'});}
+        if(typeof wit_content_type==='undefined' || wit_content_type===''){
+            return res.json(400,{message: 'content_type header is required'});}
         let url = 'https://api.wit.ai/entities/' + entity_name + '?v=20170307';
         request({
-            method: 'GET',
-            url: url,
+            method: 'GET', url: url,
             headers: {
-                'Authorization': 'Bearer 6PN2II4QPW5UYG3VPR6EXWFRU6MTTFBH',
-                'Content-Type': 'application/json'
+                'Authorization': wit_token, 'Content-Type': wit_content_type
+            }
+        }, function (err, wit_res) {
+            if (err) {return res.json(500,{message: 'Internal sever error'});}
+            return res.json(200,{data: JSON.parse(wit_res.body)});
+        });
+    },
+    putEntityById: async function(req,res){
+        print_request('putEntityById');
+        let entity_name = req.body.entity_name;
+        let wit_token = req.headers.authorization;
+        let wit_content_type = req.headers.content_type;
+        let wit_doc = req.body.wit_doc;
+        let wit_values = req.body.wit_values;
+        if(typeof entity_name==='undefined' || entity_name===''){
+            return res.json(400,{message: 'entity_name is required'});}
+        if(typeof wit_values==='undefined' || wit_values===''){
+            return res.json(400,{message: 'wit_values is required'});}
+        if(typeof wit_doc==='undefined' || wit_doc===''){
+            return res.json(400,{message: 'wit_doc is required'});}
+        if(typeof wit_token==='undefined' || wit_token===''){
+            return res.json(400,{message: 'Authorization header is required'});}
+        if(typeof wit_content_type==='undefined' || wit_content_type===''){
+            return res.json(400,{message: 'content_type header is required'});}
+        let url = 'https://api.wit.ai/entities/' + entity_name + '?v=20170307';
+        try{
+            wit_values = JSON.parse(wit_values);
+        }catch (err){
+            return res.json(400,{message: 'wit_values should be in JSON format'});
+        }
+        let body = {"doc": wit_doc, "values": wit_values};
+        request({
+            method: 'PUT', url: url,
+            json: body,
+            headers: {
+                'Authorization': wit_token, 'Content-Type': wit_content_type
             }
         }, function (err, wit_res) {
             if (err) {return res.json(500,{message: 'Internal sever error'});}
