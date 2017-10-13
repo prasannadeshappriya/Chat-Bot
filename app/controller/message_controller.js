a12b44521019809dfd6d20e46bc3473c15c3a0b8
3LOGIXS\prasanna_d <prasanna.14@cse.mrt.ac.lk>
2017-10-13 17:16:37 +0530

    Minor changes

--- app/controller/message_controller.js
+++ app/controller/message_controller.js
@@ -33,7 +33,9 @@ module.exports = {
         let sessionId;
         //get all stored sessions from the database;
         let stored_sessions = await sessionRepository.getAllSessions();
-        console.log('this is fun');
+        console.log('----------------------------------------------');
+        console.log(stored_sessions);
+        console.log('----------------------------------------------');
         if(stored_sessions[0]) {
             for (let i = 0; i < stored_sessions[1].length; i++){
                 let item = stored_sessions[1][i];
